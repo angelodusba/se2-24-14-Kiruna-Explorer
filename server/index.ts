b60827cc31c9e8923ec82a6a1198ffbe854ebed3
fede-palazz <fede.palazzi1999@gmail.com>
2024-10-29 00:20:51 +0100

    Configured dotenv in backend

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,20 @@
-const cors = require("cors");
+import cors from "cors";
 import express from "express";
 import initRoutes from "./src/routes";
 import dotenv from "dotenv";
 
-dotenv.config();
-const app: express.Application = express();
-
-const port: number = 3001;
-
+const env = dotenv.config({ path: "../.env" });
+if (env.error) {
+  throw new Error("File `.env` not found. Please create it.");
+}
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
 };
+
+const app: express.Application = express();
+const port: number = 3001;
+
 app.use(cors(corsOptions));
 
 initRoutes(app);
