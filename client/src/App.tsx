c46601cca824233170f7b3d0dc30e25b4fc64561
fede-palazz <fede.palazzi1999@gmail.com>
2024-10-28 20:34:40 +0100

    Added material icons, Roboto font, upgraded to Grid v2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import DynamicColumnForm from './components/AddDocumentForm'
+import "./App.css";
+import DynamicColumnForm from "./components/AddDocumentForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <DynamicColumnForm />
-  )
+  return <DynamicColumnForm />;
 }
 
-export default App
+export default App;
