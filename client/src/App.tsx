c859fa05cce7c51ebab5da7711300ce8ad9a1d54
angelodusba <angelodusba@gmail.com>
2024-11-01 16:05:11 +0100

    Login page setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,38 @@
 import "./App.css";
-import Map from "./components/Map";
+import Map from "./components/Map/Map";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LoginPage from "./components/Login/LoginPage";
+import { useState } from "react";
+import User from "./models/User";
+import UserContext from "./contexts/UserContext";
 
 function App() {
-  return <Map></Map>;
+  const [user, setUser] = useState<User | undefined>(undefined);
+  return (
+    <UserContext.Provider value={user}>
+      <Routes>
+        <Route path="/auth" element={<LoginPage></LoginPage>}></Route>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar></Navbar>
+              <Outlet />
+            </>
+          }>
+          <Route
+            path="/map"
+            element={
+              <>
+                <Map></Map>
+              </>
+            }
+          />
+        </Route>
+      </Routes>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
