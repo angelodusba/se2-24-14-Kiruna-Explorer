4297b50a9c5de808ee4cd9f77bf56815468064ea
Fateme <s324683@studenti.polito.it>
2024-11-15 10:35:33 +0100

    document List

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,9 +11,11 @@ import DocumentAPI from "./API/DocumentAPI";
 import AddDocumentPage from "./pages/AddDocumentPage";
 import LinkDocumentsPage from "./pages/LinkDocumentsPage";
 import DocumentList from "./components/listDocument/DocumentList";
+import { DisabledInputContext } from "./contexts/DisabledInputContext";
 
 function App() {
   const [user, setUser] = useState<User | undefined>(undefined);
+  const [disabledInput, setDisabledInput] = useState(false);
   const [docsLocation, setDocsLocation] = useState([]);
 
   const navigate = useNavigate();
@@ -49,72 +51,76 @@ function App() {
 
   return (
     <UserContext.Provider value={user}>
-      <Routes>
-        <Route
-          path="/"
-          element={user ? <Navigate to="/map" /> : <Navigate to="/auth" />}
-        />
-        <Route
-          path="/auth"
-          element={
-            user ? <Navigate to={"/map"} /> : <LoginPage login={doLogin} />
-          }
-        />
-        <Route
-          path="/"
-          element={
-            <>
-              <Navbar logout={doLogout} />
-              <Outlet />
-            </>
-          }
-        >
+      <DisabledInputContext.Provider
+        value={{ disabledInput, setDisabledInput }}
+      >
+        <Routes>
           <Route
-            path="/map"
+            path="/"
+            element={user ? <Navigate to="/map" /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/auth"
+            element={
+              user ? <Navigate to={"/map"} /> : <LoginPage login={doLogin} />
+            }
+          />
+          <Route
+            path="/"
             element={
               <>
-                <Map docs={docsLocation} />
+                <Navbar logout={doLogout} />
                 <Outlet />
               </>
             }
           >
             <Route
-              path="add"
+              path="/map"
               element={
-                user && user.role === Role.UrbanPlanner ? (
-                  <AddDocumentPage />
-                ) : (
-                  <Navigate to="/auth" />
-                )
+                <>
+                  <Map docs={docsLocation} />
+                  <Outlet />
+                </>
               }
-            />
-            <Route
-              path="link"
-              element={
-                user && user.role === Role.UrbanPlanner ? (
-                  <LinkDocumentsPage />
-                ) : (
-                  <Navigate to="/auth" />
-                )
-              }
-            />
-            <Route
-              path="documents"
-              element={
-                user ? (
-                  <DocumentList open={true} onClose={() => {}} />
-                ) : (
-                  <Navigate to="/auth" />
-                )
-              }
-            />
+            >
+              <Route
+                path="add"
+                element={
+                  user && user.role === Role.UrbanPlanner ? (
+                    <AddDocumentPage />
+                  ) : (
+                    <Navigate to="/auth" />
+                  )
+                }
+              />
+              <Route
+                path="link"
+                element={
+                  user && user.role === Role.UrbanPlanner ? (
+                    <LinkDocumentsPage />
+                  ) : (
+                    <Navigate to="/auth" />
+                  )
+                }
+              />
+              <Route
+                path="documents"
+                element={
+                  user ? (
+                    <DocumentList open={true} onClose={() => {}} />
+                  ) : (
+                    <Navigate to="/auth" />
+                  )
+                }
+              />
+            </Route>
           </Route>
-        </Route>
-        <Route
-          path="*"
-          element={user ? <Navigate to="/map" /> : <Navigate to="/auth" />}
-        />
-      </Routes>
+          <Route
+            path="*"
+            element={user ? <Navigate to="/map" /> : <Navigate to="/auth" />}
+          />
+        </Routes>
+      </DisabledInputContext.Provider>
     </UserContext.Provider>
   );
 }
