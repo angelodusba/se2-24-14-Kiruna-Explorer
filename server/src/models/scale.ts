295c01a32d339653a9ff6a5a95c8d359f0567dc9
GHADI HAJ S331542 <s331542@studenti.polito.it>
2024-11-26 15:28:56 +0100

    Implementing API for scale

---
+++ server/src/models/scale.ts
@@ -0,0 +1,12 @@
+class Scale{
+    id: number;
+    name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+      }
+}
+export default Scale;
+
+//! in create document, we should update the create document to take scale_id as a combo box (similar to type)??
