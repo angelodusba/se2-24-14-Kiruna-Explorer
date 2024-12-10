3a89c360605f3333978df2706e013f68ba96742a
FabioGigante <s328890@studenti.polito.it>
2024-12-10 23:37:00 +0100

    fixed source and target handles

--- client/src/components/Diagram/ConnectionStyles.tsx
+++ client/src/components/Diagram/ConnectionStyles.tsx
@@ -1,21 +1,21 @@
 const connectionStyles = {
     "prevision_conn": {
         stroke: "#FFA500",
-        strokeWidth: 2,
+        strokeWidth: 5,
         strokeDasharray: "5,5"
     },
     "direct_conn": {
         stroke: "#000000",
-        strokeWidth: 2,
+        strokeWidth: 5,
     },
     "collateral_conn": {
         stroke: "#0000FF",
-        strokeWidth: 2,
+        strokeWidth: 5,
         strokeDasharray: "10,5"
     },
     "update_conn": {
         stroke: "#00FF00",
-        strokeWidth: 2,
+        strokeWidth: 5,
         strokeDasharray: "5,10"
     },
     "default": {
--- client/src/components/Diagram/ZoomNode.tsx
+++ client/src/components/Diagram/ZoomNode.tsx
@@ -2,9 +2,10 @@ import React, { memo } from 'react';
 import { Handle, useStore, Position } from 'reactflow';
 import { createReactFlowIcon } from '../Map/Icons';
 import { useParams } from 'react-router-dom';
- 
+import zIndex from '@mui/material/styles/zIndex';
+
 const zoomSelector = (s) => s.transform[2] >= 0.2;
- 
+
 interface ZoomNodeProps {
   data: {
     type: string;
@@ -22,10 +23,10 @@ export default memo(({ data }: ZoomNodeProps) => {
   const opacity = selectedDocId && selectedDocId !== docId && !data.connections.some((link) => (link.id_doc1 === selectedDocId && link.id_doc2 === docId) || (link.id_doc1 === docId && link.id_doc2 === selectedDocId)) ? 0.5 : 1;
   return (
     <>
-      <Handle type="target" position={Position.Top} id="tt" style={{ width: 20, height: 10, background: 'grey', borderRadius: '1%' }} />
-      <Handle type="target" position={Position.Right} id="tr" style={{ width: 20, height: 10, background: 'grey', borderRadius: '1%' }} />
-      <Handle type="target" position={Position.Bottom} id="tb" style={{ width: 20, height: 10, background: 'grey', borderRadius: '1%' }} />
-      <Handle type="target" position={Position.Left} id="tl" style={{ width: 20, height: 10, background: 'grey', borderRadius: '1%' }} />
+      <Handle type="target" position={Position.Top} id="tt" style={{ width: 15, height: 15, background: 'green', borderRadius: '1%' }} />
+      <Handle type="target" position={Position.Right} id="tr" style={{ width: 15, height: 15, background: 'green', borderRadius: '1%' }} />
+      <Handle type="target" position={Position.Bottom} id="tb" style={{ width: 15, height: 15, background: 'green', borderRadius: '1%' }} />
+      <Handle type="target" position={Position.Left} id="tl" style={{ width: 15, height: 15, background: 'green', borderRadius: '1%' }} />
       {showContent ? (
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 'auto', height: 'auto' }}>
       {IconComponent}
@@ -33,10 +34,18 @@ export default memo(({ data }: ZoomNodeProps) => {
       ) : (
       "---"
       )}
-      <Handle type="source" position={Position.Top} id="st" style={{ width: 20, height: 10, background: 'grey', borderRadius: '1%' }} />
-      <Handle type="source" position={Position.Right} id="sr" style={{ width: 20, height: 10, background: 'grey', borderRadius: '1%' }} />
-      <Handle type="source" position={Position.Bottom} id="sb" style={{ width: 20, height: 10, background: 'grey', borderRadius: '1%' }} />
-      <Handle type="source" position={Position.Left} id="sl" style={{ width: 20, height: 10, background: 'grey', borderRadius: '1%' }} />
+      <Handle type="source" position={Position.Top} id="st" style={{ width: 15, height: 15, background: 'blue', borderRadius: '1%', marginTop: '-15px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ color: 'white', fontSize: '10px' }}>S</div>
+      </Handle>
+      <Handle type="source" position={Position.Right} id="sr" style={{ width: 15, height: 15, background: 'blue', borderRadius: '1%', marginTop: '-15px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ color: 'white', fontSize: '10px' }}>S</div>
+      </Handle>
+      <Handle type="source" position={Position.Bottom} id="sb" style={{ width: 15, height: 15, background: 'blue', borderRadius: '1%', marginBottom: '-15px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ color: 'white', fontSize: '10px' }}>S</div>
+      </Handle>
+      <Handle type="source" position={Position.Left} id="sl" style={{ width: 15, height: 15, background: 'blue', borderRadius: '1%', marginTop: '-15px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ color: 'white', fontSize: '10px' }}>S</div>
+      </Handle>
     </>
   );
 });
